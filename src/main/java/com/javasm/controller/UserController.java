a7d557404b3f1e670403f70c7448ca00df3dabc0
ZKDeGitHub <ke15290172726@gmail.com>
2023-07-10 17:19:46 +0800

    23

--- src/main/java/com/javasm/controller/UserController.java
+++ src/main/java/com/javasm/controller/UserController.java
@@ -1,6 +1,12 @@
 package com.javasm.controller;
 
+import com.javasm.entity.User;
+import com.javasm.setivce.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
 
 /**
  * @Author zk
@@ -8,5 +14,21 @@ import org.springframework.stereotype.Controller;
  * @注释：
  */
 @Controller
+@ResponseBody
 public class UserController {
+    @Resource
+    private UserService userService;
+
+    @PostMapping("add")
+    public String addUser(@RequestBody User user) {
+        System.out.println("user: " + user);
+        userService.add(user);
+        return "add success";
+    }
+
+    @GetMapping("query")
+    public User query(int id) {
+        User user = userService.queryById(id);
+        return user;
+    }
 }
